22af4d203ba5731dff3f614692d4636ecd152fdf
Knarik8 <k_armina@mail.ru>
2024-06-21 00:01:40 +0300

    fixed Training

--- src/main/java/epam/gym/entity/Training.java
+++ src/main/java/epam/gym/entity/Training.java
@@ -1,6 +1,14 @@
 package epam.gym.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
