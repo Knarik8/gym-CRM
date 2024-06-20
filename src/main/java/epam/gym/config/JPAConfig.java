220dd3a8d2a9909b04db6ce4d74b3278556f7258
Knarik8 <k_armina@mail.ru>
2024-06-21 00:04:21 +0300

    added JPAConfig

--- src/main/java/epam/gym/config/JPAConfig.java
+++ src/main/java/epam/gym/config/JPAConfig.java
@@ -3,21 +3,26 @@ package epam.gym.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
 
 @Configuration
-@Import(CommonConfig.class)
+@PropertySource("classpath:application.properties")
+@EnableTransactionManagement
 public class JPAConfig {
 
     @Value("${jdbc.driverClassName}")
@@ -81,7 +86,20 @@ public class JPAConfig {
         hibernateProperties.setProperty("hibernate.hbm2ddl.auto", hbm2ddlAuto);
         hibernateProperties.setProperty("hibernate.dialect", dialect);
         hibernateProperties.setProperty("hibernate.cache.use_second_level_cache", "false");
-
         return hibernateProperties;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+    private ResourceDatabasePopulator databasePopulator() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("fillDB.sql"));
+        return populator;
     }
 }
