dbb8074d251bde8dd991d687e23c1f7c68ca9702
Knarik8 <k_armina@mail.ru>
2024-06-27 00:45:51 +0300

    added metrics

--- src/main/java/epam/gym/controller/TestController.java
+++ src/main/java/epam/gym/controller/TestController.java
@@ -1,5 +1,7 @@
 package epam.gym.controller;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class TestController {
 
+    Counter visitCounter;
+
+    public TestController(MeterRegistry registry) {
+        visitCounter = Counter.builder("visit_counter")
+                .description("Number of visits to the /hello")
+                .register(registry);
+
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
+        visitCounter.increment();
         return "Hello, World!";
     }
 }
