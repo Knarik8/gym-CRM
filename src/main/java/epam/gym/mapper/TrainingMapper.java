439101d13750066cf53b610de1a152efe10c3909
Knarik8 <k_armina@mail.ru>
2024-08-12 23:04:05 +0300

    added Mapping

--- src/main/java/epam/gym/mapper/TrainingMapper.java
+++ src/main/java/epam/gym/mapper/TrainingMapper.java
@@ -3,6 +3,7 @@ package epam.gym.mapper;
 import epam.gym.dto.training.TrainingDto;
 import epam.gym.entity.Training;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,6 +11,14 @@ public interface TrainingMapper {
 
     TrainingMapper trainingMapper = Mappers.getMapper(TrainingMapper.class);
 
+    @Mapping(source = "trainingTypeId", target = "trainingType.id")
+    @Mapping(source = "traineeId", target = "trainee.id")
+    @Mapping(source = "trainerId", target = "trainer.id")
     Training toEntity(TrainingDto dto);
+
+    @Mapping(source = "trainingType.id", target = "trainingTypeId")
+    @Mapping(source = "trainee.id", target = "traineeId")
+    @Mapping(source = "trainer.id", target = "trainerId")
+    TrainingDto toDto(Training entity);
 
 }
