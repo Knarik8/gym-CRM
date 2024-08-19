632645fa78c215898524d03ff2fc0b6b82967881
Knarik8 <k_armina@mail.ru>
2024-08-19 13:04:49 +0300

    added Eureka

--- src/main/java/epam/gym/App.java
+++ src/main/java/epam/gym/App.java
@@ -2,9 +2,11 @@ package epam.gym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class App
 {
     public static void main( String[] args )
